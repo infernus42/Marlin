ebb3953a1bac1e486b9881dad0ef2ee4807fff06
Muhammet Furkan AKDENIZ <77023841+infernus42@users.noreply.github.com>
2021-09-24 09:56:49 +0300

    bed thermal runaway parameters changed

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -283,8 +283,8 @@
  * Thermal Protection parameters for the bed are just as above for hotends.
  */
 #if ENABLED(THERMAL_PROTECTION_BED)
-  #define THERMAL_PROTECTION_BED_PERIOD        20 // Seconds
-  #define THERMAL_PROTECTION_BED_HYSTERESIS     2 // Degrees Celsius
+  #define THERMAL_PROTECTION_BED_PERIOD        100 // Seconds
+  #define THERMAL_PROTECTION_BED_HYSTERESIS     15 // Degrees Celsius
 
   /**
    * As described above, except for the bed (M140/M190/M303).
